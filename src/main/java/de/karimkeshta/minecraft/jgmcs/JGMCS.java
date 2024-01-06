725b3850762258d81d99642047be3064226d7bc1
KarimKeshta <dev@karimkeshta.de>
2024-01-06 16:29:42 +0100

    First Commit

---
+++ src/main/java/de/karimkeshta/minecraft/jgmcs/JGMCS.java
@@ -0,0 +1,39 @@
+/*
+ * This java application was developed by Karim Keshta.
+ * Please ask me before using parts of my code.
+ * Thanks!
+ */
+package de.karimkeshta.minecraft.jgmcs;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author karim
+ */
+public class JGMCS extends JavaPlugin{
+
+    @Override
+    public void onLoad() {
+     
+        
+        
+    }
+    
+    @Override
+    public void onEnable() {
+        
+        
+        
+    }
+
+    @Override
+    public void onDisable() {
+        
+        
+        
+    }
+
+    
+    
+}
